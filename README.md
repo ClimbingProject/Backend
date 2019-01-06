e9dafbf65a1ebb5b6192a5ef3ef3c76ce1f89237
Itaru7 <ir172178@gmail.com>
2019-01-06 14:55:27 -0800

    README & signup.py
    - completed the function of search by email
    - added key commands for DynamoDB
    - changed create_table.py for searching by only email

--- README.md
+++ README.md
@@ -10,3 +10,27 @@
 
             java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
 
+You can check what's in DynamoDB by this command on the terminal
+
+    aws dynamodb list-tables --endpoint-url http://localhost:8000
+
+lsit table
+
+    aws dynamodb list-tables --endpoint-url http://localhost:8000
+
+scan items in the table
+
+    aws dynamodb scan --table-name Users --endpoint-url http://localhost:8000
+
+
+delete table
+
+    aws dynamodb delete-table --table-name Users --endpoint-url http://localhost:8000
+
+delete item from table
+
+    aws dynamodb delete-item --table-name Users --key "{\"email\": {\"S\": \"miguel@gmail.com\"}, \"password\":{\"S\": \"python\"}}" --endpoint-url http://localhost:8000
+
+get item from table
+
+    aws dynamodb get-item --table-name Users --key  "{\"email\": {\"S\": \"miguel@gmail.com\"}, \"password\":{\"S\": \"python\"}}" --endpoint-url http://localhost:8000
--- signup.py
+++ signup.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify, Blueprint, request, abort
+import boto3
+from botocore.exceptions import ClientError
 
 signup = Blueprint('signup', __name__)
 
-test_list = [{"user1": "1234", "user2": "5678"}]
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
 
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('Users')
 
 # GETの実装
 # curl -i http://0.0.0.0:3001/get
@@ -13,17 +22,31 @@ def get():
     return jsonify(result)
 
 
-# curl -i -X POST -H "Content-Type: application/json" -d '{"username":"miguel","password":"python"}' http://0.0.0.0:3001/signup
+# curl -i -X POST -H "Content-Type: application/json" -d '{"email":"miguel@gmail.com","password":"python"}' http://0.0.0.0:3001/signup
 @signup.route('/signup', methods=['POST'])
 def new_user():
-    username = request.json.get('username')
+    email = request.json.get('email')
     password = request.json.get('password')
-    if username is None or password is None:
+    if email is None or password is None:
         abort(400) # missing arguments
-    if username in test_list[0]:
-        abort(400) # existing user
-    user = username
-    # user.hash_password(password)
-    # db.session.add(user)
-    # db.session.commit()
-    return jsonify({ 'username': user }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+    try:
+        response = table.get_item(Key={"email": email})#, "password": password})
+        print(response)
+        if 'Item' in response:
+            abort(400) # existing user
+        table.put_item(
+            Item={
+                'email': email,
+                'password': password
+            }
+        )
+        # user.hash_password(password)
+        # db.session.add(user)
+        # db.session.commit()
+        return jsonify({ 'ok': email }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+
+#"{\"email\":{\"S\": \"miguel@gmail.com\"}}"
